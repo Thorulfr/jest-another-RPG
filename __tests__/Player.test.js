45457d0efb5c53080942812f6772476ee657ae8e
Ben H <deepfreespace@gmail.com>
2021-11-10 23:42:53 -0700

    Mocks

---
+++ __tests__/Player.test.js
@@ -0,0 +1,16 @@
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
+
+test('Creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
