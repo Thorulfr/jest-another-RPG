3c458679449ce7272c53776f56a607d7f4c20df1
Ben H <deepfreespace@gmail.com>
2021-11-11 20:41:52 -0700

    Some player prototypes

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -31,3 +31,29 @@ test("Gets player's inventory or returns false", () => {
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
 });
+
+test("Gets player's health", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(
+        expect.stringContaining(player.health.toString())
+    );
+});
+
+test('Checks if player is alive', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("Lowers player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+});
